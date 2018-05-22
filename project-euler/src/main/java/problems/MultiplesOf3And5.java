a0afb6ca8046ddc9f410fb9e2d477a368c29b221
mustaphaz <mustapha.zeamari@alliander.com>
2018-05-22 12:57:38 +0200

    multiplesof3and5 using stream

--- project-euler/src/main/java/problems/MultiplesOf3And5.java
+++ project-euler/src/main/java/problems/MultiplesOf3And5.java
@@ -1,8 +1,11 @@
 package problems;
 
+import java.util.stream.IntStream;
+
 public class MultiplesOf3And5 {
 
-    static int calculateMultiplesOf3And5(int end) {
+    // the old way:
+    int calculateMultiplesOf3And5(int end) {
         int sum = 0;
         for (int i = 0; i < end; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
@@ -12,8 +15,20 @@ public class MultiplesOf3And5 {
         return sum;
     }
 
+    // the new way:
+    private int calculateMultiplesOf3And5Stream(int end) {
+        return IntStream.range(1, end)
+                .filter(this::isDivisibleBy3Or5)
+                .sum();
+    }
+
+    private boolean isDivisibleBy3Or5(int number){
+        return number % 3 == 0 || number % 5 == 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println(calculateMultiplesOf3And5(1000000000));
+        System.out.println(new MultiplesOf3And5().calculateMultiplesOf3And5(1000000000));
+        System.out.println(new MultiplesOf3And5().calculateMultiplesOf3And5Stream(1000000000));
     }
 
 }
